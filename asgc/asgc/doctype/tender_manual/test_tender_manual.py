ba875a40ecf86fa2e30d4e25f1b94c00a8cf9f8d
9050nasser <9050nasser@gmail.com>
2023-11-15 16:00:17 +0000

    first

---
+++ asgc/asgc/doctype/tender_manual/test_tender_manual.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, Mohammed Nasser and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestTenderManual(FrappeTestCase):
+	pass
